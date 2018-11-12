152ee4da480e3fe77656474dc86f3484bd392391
Kirize14 (PNK148) <prawich.14@gmail.com>
2018-11-12 11:26:39 +0700

    add show data file

---
+++ web/data.php
@@ -0,0 +1,247 @@
+<?php
+    //SET DEFAULT TIMEZONE
+    date_default_timezone_set("Asia/Bangkok");
+
+    //Connect to database
+    $host = "localhost";
+    $user = "DB_USER";
+    $pass = "DB_PASS";
+    $daba = "DATABASE NAME";
+
+    //Get data from database
+    $con = mysqli_connect($host,$user,$pass,$daba);
+    $query = "SELECT * FROM (SELECT * FROM `egg` ORDER BY `egg`.`id` DESC LIMIT 50) egg ORDER BY `egg`.`id`";
+    $query = mysqli_query($con,$query);
+    while($data = mysqli_fetch_array($query)){
+        $humidity[] =  $data['humidity'];
+        $date[] =  $data['date'];
+        $temp[] =  $data['temp'];
+     }
+     
+?>
+
+<!-- Some website stuff -->
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Kinaree Egg</title>
+
+    <!-- Bootstrap core CSS-->
+    <link href="https://shareport.ikirize.net/client/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template-->
+    <link href="https://shareport.ikirize.net/client/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Page level plugin CSS-->
+    <link href="https://shareport.ikirize.net/client/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="https://shareport.ikirize.net/client/css/sb-admin.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+      <a class="navbar-brand mr-1" href="#">Kinaree</a>
+
+
+
+      
+
+    </nav>
+
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+      
+      <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="#">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Charts</li>
+          </ol>
+
+          <!-- Area Chart Example-->
+          <div class="card mb-3">
+            <div class="card-header">
+              Humidity</div>
+            <div class="card-body">
+              <canvas id="myAreaChart" width="100%" height="30"></canvas>
+            </div>
+          </div>
+          <div class="card mb-3">
+            <div class="card-header">
+              Tempurature</div>
+            <div class="card-body">
+              <canvas id="myAreaChart2" width="100%" height="30"></canvas>
+            </div>
+          </div>
+
+          
+
+          
+
+        </div>
+        <!-- /.container-fluid -->
+
+        <!-- Sticky Footer -->
+        <footer class="">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright © Kinaree 2018</span>
+            </div>
+          </div>
+        </footer>
+
+      </div>
+      <!-- /.content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="https://shareport.ikirize.net/client/vendor/jquery/jquery.min.js"></script>
+    <script src="https://shareport.ikirize.net/client/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="https://shareport.ikirize.net/client/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Page level plugin JavaScript-->
+    <script src="https://shareport.ikirize.net/client/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="https://shareport.ikirize.net/client/js/sb-admin.min.js"></script>
+
+    <!-- Demo scripts for this page-->
+    <script>
+        // Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: <?php echo json_encode($date); ?>,
+    datasets: [{
+      label: "Humidity",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: <?php echo json_encode($humidity); ?>, //[1,2,3,4,5,6]
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart2");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: <?php echo json_encode($date); ?>,
+    datasets: [{
+      label: "Temp",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: <?php echo json_encode($temp); ?>,
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+    
+    </script>
+    <script src="https://shareport.ikirize.net/client/js/demo/chart-bar-demo.js"></script>
+    <script src="https://shareport.ikirize.net/client/js/demo/chart-pie-demo.js"></script>
+
+  </body>
+
+</html>
+
