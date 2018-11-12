cd1e365be225e1dd86a6ddf30a35353588d3b615
BuildTools <unconfigured@null.spigotmc.org>
2018-11-12 11:21:23 +0700

    Upload data to server file

--- web/somthing.php
+++
@@ -1,1 +0,0 @@
-kknaksjdcfnkasjfnkjfnkrjcgnqerjfdaoskf;s
---
+++ web/upload.php
@@ -0,0 +1,21 @@
+<?php
+    $humidity = $_GET['humidity'];
+    $temp = $_GET['temperature'];
+    date_default_timezone_set("Asia/Bangkok");
+
+    $host = "localhost";
+    $user = "DB_USER";
+    $pass = "DB_PASS";
+    $daba = "Database name";
+
+    $con = mysqli_connect($host,$user,$pass,$daba);
+    $query = "INSERT INTO egg (id, humidity, temp, date) VALUES (NULL, '".$humidity."', '".$temp."','".date("Y/m/d h:i:sa")."');";
+    $query = mysqli_query($con,$query);
+
+
+    echo 'inserted';
+    
+
+	//USE CASE  -> localhost/upload.php?humidity="Your humidity"&temperature="Your Temp"
+?>
+
