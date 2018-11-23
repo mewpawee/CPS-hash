b755c8ee0d42cce7296c94ca8874deb92cac5a64
paweemew <32890875+paweemew@users.noreply.github.com>
2018-11-23 16:39:59 +0700

    Add comments and make A backup initialize setting in case having a communication problems

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -4,6 +4,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
 //------------------------//
+
 /*
  * mapping
  * DHT          pin 2
@@ -13,7 +14,9 @@
  * servo        pin 6
  * water sensor pin 7
  * humidifier   pin 8 (to relay)
+ * ground alike pin 13
 */
+
 //------ define some variables(pin)---//
 #define DHTTYPE DHT22
 #define DHTPIN 2
@@ -22,13 +25,16 @@
 #define fan1 5
 #define water 7
 #define humidifier 8
+#define ground 13
 //---------------------------//
+
 //----------declare some variables------//
 Servo myservo;                   // declare myservo as Servo                                                                                                                                         
 DHT dht(DHTPIN, DHTTYPE);        // declare dht as DHT at pin 2 type DHT22
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Set the LCD I2C address
 HardwareSerial & commu = Serial; // declare commu as Serial
 //--------------------------------------//
+
 //----------declare some global variables------//
   float hum_kg = 0.00;
   float hum_err = 0.00;
@@ -39,7 +45,8 @@ HardwareSerial & commu = Serial; // declare commu as Serial
   float bhum = 0.00;
   float btemp = 0.00;
 //--------------------------------------//
-char fromEsp[22]; //Initialized variable to store recieved data from ESPino32
+
+char fromEsp[18]; //Initialized variable to store recieved data from ESPino32
 
 /*Initiate part*/
 void setup(){
@@ -49,50 +56,59 @@ void setup(){
   pinMode(water, INPUT);      // set water as input
   pinMode(light,OUTPUT);      // set light as output
   pinMode(humidifier,OUTPUT); // set humidifier as output
-  pinMode(fan,OUTPUT);        // set fan as input
-  pinMode(fan1,OUTPUT);       // set fan1 as input
+  pinMode(fan,OUTPUT);        // set fan as output
+  pinMode(fan1,OUTPUT);       // set fan1 as output
+  pinMode(ground,OUTPUT);     // set ground as output
   myservo.attach(6);          // attach servo to pin 6 
   
-  /*greeting*/
+  /*greeting with "KINNAREE" */
   lcd.setCursor(0,0);
   lcd.print("KINNAREE");
   delay(1000);
   lcd.clear();
 
+  /*turn pin 13 to ground*/
+  digitalWrite(ground,LOW);
+  
   /*start fan*/
   digitalWrite(fan, HIGH);
   digitalWrite(fan1, LOW);
 }  
 
+
 /*main loop*/
 void loop(){
+  
   /*get data from sensors*/
   float temp = dht.readTemperature(); // read temperature for dht
   float hum = dht.readHumidity();   // read humidity from dht
   float water_status = digitalRead(water);  // read water from water sensor
-    
-  /* reading part*/
-  commu.readBytes(fromEsp,22);
-  String message = String(fromEsp);
-  float set_hum = message.substring(0,5).toFloat();
-  float set_temp = message.substring(6,11).toFloat();
-  int set_fan_di = message.substring(12,13).toInt();
-  int set_fan = message.substring(14,15).toInt(); 
-  int set_light = message.substring(16,17).toInt();
-  int set_humidifier = message.substring(18,19).toInt();
-  int set_tray = message.substring(20,21).toInt();
   
-  /*mock data*/
-  int   fan_di = 1;             //0 in,1 out
-  int   fan_status = 2;         //0 off, 1 on
-  int   light_status = 3;       //0 off, 1 on
-  int   humidifier_status = 4;  //0 off, 1 on
-  int   tray_pos = 0;           //0 front 1 back
+  /*Initialize setting for incubator, in case lost connect to ESPINO32*/
+  float maxHum = 58.00;           //max Humadity
+  float maxTemp = 37.00;          //max Temperature
+  int   tray_pos = 0;             //Tray position (0 off, 1 on)
+  int   light_status = 1;         //light status (0 off, 1 on)
+  int   humidifier_status = 1;    //humidifier status
   
-  /*set variables*/
-  float maxHum = 58.00; // Will get this value from esp32 later
-  float maxTemp = 37.00; // Will get this value from esp32 later
+  /* Reading from ESP*/
+  while(commu.available()){           
+     commu.readBytes(fromEsp,18);
+      String message = String(fromEsp);                           //Change what we get from ESP to string
+      float set_hum = message.substring(0,5).toFloat();           //Cut 0-4 positions of String to be set_hum
+      float set_temp = message.substring(6,11).toFloat();         //Cut 6-10 positions of String to be set_temp
+      int set_light = message.substring(12,13).toInt();           //Cut 12 position of String to be set_light
+      int set_humidifier = message.substring(14,15).toInt();      //Cut 14 position of String to be set_humidifier
+      int set_tray = message.substring(16,17).toInt();            //Cut 16 position of String to be set_tray
 
+      /*set variables according to Espino32*/
+      light_status = set_light;                
+      humidifier_status = set_humidifier;      
+      tray_pos = set_tray;                     
+      maxHum = set_hum;                         
+      maxTemp = set_temp;                       
+  }
+  
   /*Check if is NAN then use the backup value*/
   if(isnan(temp)||isnan(hum)){
     temp = btemp;
@@ -101,30 +117,25 @@ void loop(){
     btemp = temp;
     bhum = hum;
   }
-  
+
   /*use kalman filter*/
-  kalman_filter(temp, temp_kg, 5.00, temp_err, temp_est); //use tool error as 5 percents
-  kalman_filter(hum, hum_kg, 5.00, hum_err, hum_est);//use tool error as 5 percents
+  kalman_filter(temp, temp_kg, 5.00, temp_err, temp_est);   //use tool error as 5 percents
+  kalman_filter(hum, hum_kg, 5.00, hum_err, hum_est);       //use tool error as 5 percents
   
   /*Control System*/
   check_temp(temp,maxTemp);
   /*Start to Control Humidity when temperature is stable*/
-  if(abs(temp-maxTemp)<1.00){
+  if(abs(temp-maxTemp)<0.50){
      check_humid(hum,maxHum);
+  }else{
+     digitalWrite(humidifier,LOW);
     }
-  egg_position(tray_pos);
-
-  /*show what we get from espino32*/
-//  commu.println(set_hum);
-//  commu.println(set_temp);
-//  commu.println(set_fan_di);
-//  commu.println(set_fan);
-//  commu.println(set_light);
-//  commu.println(set_humidifier);
-//  commu.println(set_tray);
-
-  /* sending part*/   
-  String msg = String(hum) + "/" + String(temp) + "/" + String(fan_di) + "/" + String(fan_status) + "/" + String(light_status) + "/" + String(humidifier_status) + "/" + String(tray_pos)+"/";
+  egg_position(tray_pos); //turn egg tray
+  light_status = digitalRead(light);                  //get light_status
+  humidifier_status = digitalRead(humidifier);        //get humidifier_status
+  
+  /* sending data to ESPINO32*/   
+  String msg = String(hum) + "/" + String(temp) + "/" + String(light_status) + "/" + String(humidifier_status) + "/" + String(tray_pos)+"/";
   const char *toEsp = msg.c_str();
   commu.write(toEsp);
   
@@ -139,19 +150,24 @@ void loop(){
   lcd.print(water_status);
   lcd.print(" P: ");
   lcd.print(tray_pos);
-  
+  lcd.setCursor(0,2);
+  lcd.print("L: ");
+  lcd.print(light_status);
+  lcd.print(" HF: ");
+  lcd.print(humidifier_status);
   delay(1000);
 }
 
 
 //---------------some functions-----------------//
-
+/*Kalman Filter, eliminate noise and make it more accurate*/
 void kalman_filter(float mea, float KG, float err_mea, float err_est, float EST){
       KG = err_est/(err_est+err_mea);
       EST = EST + KG*(mea-EST);
       err_est = (1-KG)*err_est;
   }
-     
+  
+/*Turn motor according to egg position*/     
 void egg_position(int pos){
   //pos == 0 for front position and pos ==1 for back
   if(pos == 0){
@@ -161,6 +177,7 @@ void egg_position(int pos){
     }
 }     
 
+/*Check if we should turn the humidifier on or not*/
 void check_humid(float humid, float maxHum){
     if (humid<= maxHum){
       digitalWrite(humidifier,HIGH);
@@ -169,6 +186,7 @@ void check_humid(float humid, float maxHum){
     }
 }
 
+/*Check if we should turn ther temperature on or not*/
 void check_temp(float temp, float maxTemp){
   if(temp <= maxTemp){
     digitalWrite(light, HIGH); 
--- espino32/espino32.ino
+++ espino32/espino32.ino
@@ -1,13 +1,13 @@
+// include outside libraries
 #include "Camera_Exp.h"
 #include <WiFi.h>
-
-char fromArd[22]; //Initialized variable to store recieved data from Arduino
-HardwareSerial & commu = Serial;
-
-//------------new----------------
+//------------------------//
+char fromArd[18]; //Initialized variable to store recieved data from Arduino
+HardwareSerial & commu = Serial; // declare commu as Serial
 CAMERA cam;
-char ssid[] = "mine";      //  your network SSID (name)
-char pass[] = "12345678";   // your network password
+
+char ssid[] = "CIE2";      //  your network SSID (name)
+char pass[] = "1212312121";   // your network password
 WiFiServer server(80);
 
 String http_header = "HTTP/1.1 200 OK\r\n";
@@ -34,38 +34,37 @@ void setup() {
 }
 
 void loop() {
-  //Rx and change it to variable
-  commu.readBytes(fromArd,22);
-  String message = String(fromArd);
-  float hum = message.substring(0,5).toFloat();
-  float temp = message.substring(6,11).toFloat();
-  int fan_di = message.substring(12,13).toInt();
-  int fan_status = message.substring(14,15).toInt(); 
-  int light_status = message.substring(16,17).toInt();
-  int humidifier_status = message.substring(18,19).toInt();
-  int tray_pos = message.substring(20,21).toInt();
-
-  float set_humid = 57.50;
-  float set_temp = 37.50;
-  int   set_fan_di = 6;
-  int   set_fan = 7;
-  int   set_light  = 8;
-  int   set_humidifier = 9;
+  /*Initialize data if getting trouble on connection*/
+  float hum = 0.00;
+  float temp = 0.00;
+  int light_status = 0;
+  int humidifier_status = 0;
+  int tray_pos = 0;
+  
+  /* Reading from Arduino*/
+  commu.readBytes(fromArd,18);    //reading data from Arduino
+  if(*fromArd != '\0'){
+    String message = String(fromArd);                      //Change what we get from Arduino to string
+    hum = message.substring(0,5).toFloat();                //Cut 0-4 positions of String to be hum
+    temp = message.substring(6,11).toFloat();              //Cut 6-10 positions of String to be temp
+    light_status = message.substring(12,13).toInt();       //Cut 12 positions of String to be light_status
+    humidifier_status = message.substring(14,15).toInt();  //Cut 14 positions of String to be humidifier_status
+    tray_pos = message.substring(16,17).toInt();           //Cut 16 positions of String to be tray_pos
+  }
+  
+  //set what to send back to arduino(get from website)
+  float set_humid = 58.00;
+  float set_temp = 37.00;
+  int   set_light  = 1;
+  int   set_humidifier = 0;
   int   set_tray =0;  
   
-  String msg = String(set_humid)+"/"+String(set_temp)+"/"+String(set_fan_di)+"/"+String(set_fan)+"/"+String(set_light)+"/"+String(set_humidifier)+"/"+String(set_tray)+"/";
+  /* sending data to Arduino*/   
+  String msg = String(set_humid)+"/"+String(set_temp)+"/"+String(set_light)+"/"+String(set_humidifier)+"/"+String(set_tray)+"/";
   const char *toArd = msg.c_str();
   commu.write(toArd);
-// Print variables  
-//  commu.println(hum);
-//  commu.println(temp);
-//  commu.println(fan_di);
-//  commu.println(fan_status);
-//  commu.println(light_status);
-//  commu.println(humidifier_status);
-//  commu.println(tray_pos);
 
-//----------new-----------//
+  /*create it own server*/
   String httpreq;
   client = server.available();
   if (client)
@@ -80,11 +79,6 @@ void loop() {
         httpreq += httpreq_line;
         if (httpreq_line == "\r")
         {
-          if (httpreq.indexOf("GET /stream") != -1)
-          {
-                Serial.println("Stream");
-                stream();
-          }
           if (httpreq.indexOf("GET /capture") != -1)
           {
                 Serial.println("Capture");
@@ -103,7 +97,7 @@ void loop() {
 }  
 //----------------------------//
 
-//----function to capture-----//
+//----function to capture a picture-----//
 void capture()
 {
    esp_err_t err;
